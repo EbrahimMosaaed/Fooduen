0ef3210517b57e853c6631a732146ca7ffd7f171
Ebrahim Mosaaed <eb.mahmoud@outlook.com>
2019-09-22 18:36:46 +0200

    Update main.dart
    title changes

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'FOodfuo',
+      title: 'FOoduno',
       
       routes: {
         '/home': (context) => HomePage(),
